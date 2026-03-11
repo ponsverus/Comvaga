f38a901f6bdd74dc7975288749dc2b00439ea10e
ponsverus <ponsverus@gmail.com>
2026-03-11 19:52:22 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto Condensed', 'sans-serif'],
+      },
+      borderRadius: {
+        custom: '3px',
+        button: '9999px',
+      },
+      colors: {
+        primary: '#FFD700',
+        dark: {
+          100: '#1a1a1a',
+          200: '#0d0d0d',
+          300: '#000000',
+        },
+
+        vbg:      'var(--vbg)',
+        vcard:    'var(--vcard)',
+        vcard2:   'var(--vcard2)',
+        vtext:    'var(--vtext)',
+        vsub:     'var(--vsub)',
+        vmuted:   'var(--vmuted)',
+        vborder:  'var(--vborder)',
+        vborder2: 'var(--vborder2)',
+      }
+    },
+  },
+  plugins: [],
+}
