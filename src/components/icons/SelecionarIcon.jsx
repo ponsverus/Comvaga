3810a177803cfd199d680fa62cb2c95f6463358d
ponsverus <ponsverus@gmail.com>
2026-04-21 16:20:56 -0300

    Create SelecionarIcon.jsx

---
+++ src/components/icons/SelecionarIcon.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import svg from '../../assets/icons/selecionar.svg?raw';
+import InlineSvgIcon from './InlineSvgIcon';
+
+export default function SelecionarIcon(props) {
+  return <InlineSvgIcon svg={svg} {...props} />;
+}
