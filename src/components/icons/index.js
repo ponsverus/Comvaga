536b5a1f87a2af36c2dfba4eecb33d37d5367c82
ponsverus <ponsverus@gmail.com>
2026-04-21 16:09:47 -0300

    Create index.js

---
+++ src/components/icons/index.js
@@ -0,0 +1,5 @@
+export { default as AgendamentosIcon } from './AgendamentosIcon';
+export { default as EnderecoIcon } from './EnderecoIcon';
+export { default as NegocioVerificadoIcon } from './NegocioVerificadoIcon';
+export { default as SelecionarIcon } from './SelecionarIcon';
+export { default as UsersIcon } from './UsersIcon';
