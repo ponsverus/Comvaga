82d3383ea3c741ae23a4b3e57eb21c628d02332d
ponsverus <ponsverus@gmail.com>
2026-04-21 20:28:14 -0300

    Update EnderecoIcon.jsx

--- src/components/icons/EnderecoIcon.jsx
+++ src/components/icons/EnderecoIcon.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
-import svg from '../../assets/icons/endereco.svg?raw';
-import InlineSvgIcon from './InlineSvgIcon';
 
-export default function EnderecoIcon(props) {
-  return <InlineSvgIcon svg={svg} {...props} />;
-}
+  export default function EnderecoIcon({ className = '', title, style = {}, ...props }) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className={className}
+        style={{ display: 'inline-block', ...style }}
+        aria-hidden={title ? undefined : 'true'}
+        role={title ? 'img' : 'presentation'}
+        {...props}
+      >
+        {title && <title>{title}</title>}
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M12 2C8.686 2 6 4.686 6 8c0 4.5 6 13 6 13s6-8.5 6-13c0-3.314-2.686-6-6-6Z"
+        />
+        <circle cx="12" cy="8" r="2.2" fill="white" opacity="0.85" />
+      </svg>
+    );
+  }
