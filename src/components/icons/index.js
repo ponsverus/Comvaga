d405bca11c334a36cedcef5cddd2647518dbc11b
ponsverus <ponsverus@gmail.com>
2026-04-24 08:41:49 -0300

    Update index.js

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,4 +1,5 @@
 export { default as AgendamentosIcon } from './AgendamentosIcon';
+export { default as CheckIcon } from './CheckIcon';
 export { default as EnderecoIcon } from './EnderecoIcon';
 export { default as NegocioVerificadoIcon } from './NegocioVerificadoIcon';
 export { default as SelecionarIcon } from './SelecionarIcon';
