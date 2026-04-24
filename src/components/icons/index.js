7102e0c02db2b7078539c3a748d15d7458eeb3d0
ponsverus <ponsverus@gmail.com>
2026-04-24 14:00:25 -0300

    Update index.js

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,7 +1,11 @@
 export { default as AgendamentosIcon } from './AgendamentosIcon';
 export { default as CheckIcon } from './CheckIcon';
+export { default as CrownIcon } from './CrownIcon';
 export { default as EnderecoIcon } from './EnderecoIcon';
 export { default as NegocioVerificadoIcon } from './NegocioVerificadoIcon';
 export { default as ProtectionIcon } from './ProtectionIcon';
 export { default as SelecionarIcon } from './SelecionarIcon';
+export { default as TimeIcon } from './TimeIcon';
+export { default as TimePastIcon } from './TimePastIcon';
+export { default as TrendingUpIcon } from './TrendingUpIcon';
 export { default as UsersIcon } from './UsersIcon';
