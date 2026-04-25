471ce26d102bb5375d0133e2dee78cd242e105d6
ponsverus <ponsverus@gmail.com>
2026-04-24 21:44:20 -0300

    Update TimePastIcon.jsx

--- src/components/icons/TimePastIcon.jsx
+++ src/components/icons/TimePastIcon.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function TimeQuarterToIcon({
+export default function TimePastIcon({
   className = '',
   title,
   style = {},
@@ -18,14 +18,12 @@ export default function TimeQuarterToIcon({
     >
       {title ? <title>{title}</title> : null}
 
-      <circle cx="256" cy="48" r="20" />
-      <circle cx="464" cy="256" r="20" />
-      <circle cx="256" cy="464" r="20" />
-      <circle cx="48" cy="256" r="20" />
+      <path d="M256,48C141.13,48,48,141.13,48,256H16l48,48l48-48H80c0-97.05,78.95-176,176-176s176,78.95,176,176
+      s-78.95,176-176,176c-48.63,0-92.68-19.79-124.53-51.67l-22.63,22.63C145.27,438.6,198.49,464,256,464
+      c114.87,0,208-93.13,208-208S370.87,48,256,48z" />
 
-      <circle cx="256" cy="256" r="12" />
-
-      <path d="M256 140v120l90 50" />
+      <path d="M256,144c-8.84,0-16,7.16-16,16v104c0,4.24,1.69,8.31,4.69,11.31l72,72l22.63-22.63L272,251.31V160
+      C272,151.16,264.84,144,256,144z" />
     </svg>
   );
 }
