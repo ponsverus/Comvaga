4b0bbe1830cb2755b626e81b3db2be0f440f2691
ponsverus <ponsverus@gmail.com>
2026-04-29 11:36:11 -0300

    Update AgendamentosSection.jsx

--- src/pages/dashboard/sections/AgendamentosSection.jsx
+++ src/pages/dashboard/sections/AgendamentosSection.jsx
@@ -69,7 +69,7 @@ export default function AgendamentosSection({
             </div>
           ))}
         </div>
-      ) : <p className="text-gray-500 text-center py-12">:(</p>}
+      ) : <p className="text-gray-500 text-sm font-medium">NENHUM AGENDAMENTO HOJE OU FUTURO</p>}
     </div>
   );
 }
