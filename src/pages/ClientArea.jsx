1802a4486d021c5b03ae2de277559525a468b700
ponsverus <ponsverus@gmail.com>
2026-05-03 21:59:47 -0300

    Update ClientArea.jsx

--- src/pages/ClientArea.jsx
+++ src/pages/ClientArea.jsx
@@ -810,7 +810,7 @@ export default function ClientArea({ user, onLogout }) {
                       value={emailVisivel ? novoEmail : maskedPrivateValue}
                       onChange={(e) => setNovoEmail(e.target.value)}
                       readOnly={!emailVisivel}
-                      className="w-full bg-transparent px-0 py-2 text-[14px] text-white uppercase truncate pr-4 placeholder-gray-600 outline-none focus:text-white"
+                      className={`${inputClass} uppercase max-w-[calc(100vw-13.75rem)] truncate pr-4 sm:max-w-none sm:pr-0`}
                       placeholder="E-MAIL DE ACESSO"
                     />
                   </div>
