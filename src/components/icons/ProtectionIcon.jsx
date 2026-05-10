74b9417ed5e37b491b26201e8121b40d2bb39b10
ponsverus <ponsverus@gmail.com>
2026-05-10 20:21:24 -0300

    Update ProtectionIcon.jsx

--- src/components/icons/ProtectionIcon.jsx
+++ src/components/icons/ProtectionIcon.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-export default function ShieldIcon({ className = '', title, style = {}, ...props }) {
+export default function ShieldIcon({
+  className = '',
+  title,
+  style = {},
+  ...props
+}) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill="currentColor"
       className={className}
       style={{ display: 'inline-block', ...style }}
       aria-hidden={title ? undefined : 'true'}
@@ -13,11 +17,12 @@ export default function ShieldIcon({ className = '', title, style = {}, ...props
       {...props}
     >
       {title && <title>{title}</title>}
+
       <path
         d="M12 2.5C9.5 2.5 6.5 3.5 5 4.5V12.5C5 16.5 8 20 12 21.5C16 20 19 16.5 19 12.5V4.5C17.5 3.5 14.5 2.5 12 2.5Z"
-        opacity="1"
-        stroke="currentColor"
-        strokeWidth="0.75"
+        fill="#FDE68A"
+        stroke="#EAB308"
+        strokeWidth="0.9"
         strokeLinejoin="round"
         strokeLinecap="round"
       />
