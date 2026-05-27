5ce812dcca1ccec05441bb9b85518563b55be9f9
ponsverus <ponsverus@gmail.com>
2026-05-27 16:25:54 -0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
@@ -22,11 +23,19 @@ export default [
       },
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.flat.recommended.rules,
+      ...react.configs.flat['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
@@ -34,6 +43,7 @@ export default [
       ],
       'no-unused-vars': 'off',
       'no-empty': 'warn',
+      'react/prop-types': 'off',
       'react-hooks/exhaustive-deps': 'warn',
     },
   },
