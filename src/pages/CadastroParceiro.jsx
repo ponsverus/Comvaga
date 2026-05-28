799142e58d7cf6912eab08ffadfe8018cd7462ae
ponsverus <ponsverus@gmail.com>
2026-05-28 11:44:22 -0300

    Update CadastroParceiro.jsx

--- src/pages/CadastroParceiro.jsx
+++ src/pages/CadastroParceiro.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { Eye, EyeOff } from 'lucide-react';
 import { supabase } from '../supabase';
