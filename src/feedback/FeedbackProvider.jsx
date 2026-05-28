7cd0c24035f91d34962fdc3e949d02178d60ee2c
ponsverus <ponsverus@gmail.com>
2026-05-28 08:44:10 -0300

    Update FeedbackProvider.jsx

--- src/feedback/FeedbackProvider.jsx
+++ src/feedback/FeedbackProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import FullScreenMessage from './ui/FullScreenMessage';
 import { getFeedbackState, subscribeFeedback } from './feedbackStore';
