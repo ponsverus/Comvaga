01b92a006277b32838fd76743d8773c0563a3537
ponsverus <ponsverus@gmail.com>
2026-05-28 13:41:17 -0300

    Update LoginParceiro.jsx

--- src/pages/LoginParceiro.jsx
+++ src/pages/LoginParceiro.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { Eye, EyeOff } from 'lucide-react';
 import { supabase } from '../supabase';
