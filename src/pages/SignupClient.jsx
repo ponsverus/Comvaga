91d22e0414957fa2f861540708d593ecd3ea91cd
ponsverus <ponsverus@gmail.com>
2026-05-28 15:43:41 -0300

    Update SignupClient.jsx

--- src/pages/SignupClient.jsx
+++ src/pages/SignupClient.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ArrowLeft, Eye, EyeOff } from 'lucide-react';
 import { supabase } from '../supabase';
